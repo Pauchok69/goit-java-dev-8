fd93c706f253c63ac874261ada243d745512d3fd
Andy <andrew.zerko@gmail.com>
2023-03-20 12:20:47 +0200

    Add timezone query parameter to show dynamic time and add webfilter for validation

--- src/main/java/org/example/TimeServlet.java
+++ src/main/java/org/example/TimeServlet.java
@@ -10,15 +10,19 @@ import java.io.IOException;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Optional;
 
 @WebServlet(urlPatterns = "/time")
 public class TimeServlet extends HttpServlet {
     public static final String DEFAULT_TIME_ZONE = "UTC";
     public static final String DATE_TIME_FORMAT = "yyyy-MM-dd hh-mm-ss VV";
+    public static final String QUERY_PARAM_TIMEZONE = "timezone";
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        ZonedDateTime now = ZonedDateTime.now(ZoneId.of(DEFAULT_TIME_ZONE));
+        Optional<String> timezoneOptional = Optional.ofNullable(req.getParameter(QUERY_PARAM_TIMEZONE));
+        ZoneId zoneId = ZoneId.of(timezoneOptional.orElse(DEFAULT_TIME_ZONE));
+        ZonedDateTime now = ZonedDateTime.now(zoneId);
 
         resp.setContentType("text/html");
         resp.setCharacterEncoding("utf-8");
